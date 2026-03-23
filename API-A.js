6f923394b3d35bedc47e064972ecf65cfb781312
MartinLeroy <erreurctos@gmail.com>
2026-03-23 08:19:18 +0100

    init eval

---
+++ API-A.js
@@ -0,0 +1,10 @@
+CameraA;
+class CameraA {
+  constructor(location) {}
+  onDetect(callback) {}
+}
+TemperatureSensorA;
+class TemperatureSensorA {
+  constructor(location, threshold) {}
+  onDetect(callback) {}
+}
---
+++ API-B.js
@@ -0,0 +1,5 @@
+class ThermalSensorB {
+ constructor(position) {} { // position = id de la pièce
+ triggerHeatSignature(process) {} // Déclenche une donnée complexe json, voir ci
+dessous
+}
